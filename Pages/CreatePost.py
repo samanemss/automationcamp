c67bd7e3ea12289607c96a708477a6a95ebc70d1
Samaneh <samane_mss@yahoo.com>
2022-05-10 18:16:31 +0430

    Login.py SignUp.py Signupvatest.py CreatePost.py

--- Pages/CreatePost.py
+++ Pages/CreatePost.py
@@ -1,7 +1,7 @@
 from Pages.Locators import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-# https://community.powerbi.com/t5/Data-Stories-Gallery/Meat-to-eat-or-not-to-eat-Narrative-statistics/td-p/2177919s
+
 
 class CreatePost:
     def __init__(self, driver):
@@ -13,7 +13,36 @@ class CreatePost:
     def enter_title(self, title):
         self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[1]/input').send_keys(title)
 
-    def select_source(self):
-        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/select').Select()
+    def select_source_powerbi(self):
+        source = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/select')
+        drop = Select(source)
+        drop.select_by_value("powerbi")
+
+    def enter_publicurl(self, publicurl):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[3]/input').send_keys(publicurl)
+
+    def enter_originalauthor(self, originalauthor):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[4]/input').send_keys(originalauthor)
+
+    def enter_description(self, descriptions):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[5]/textarea').send_keys(descriptions)
+
+    def enter_tags(self, tags):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[6]/input').send_keys(tags)
+
+    def select_whocanseepost(self):
+        seepost = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[7]/select')
+        dropseepost = Select(seepost)
+        dropseepost.select_by_value("private")
+
+    def enter_license(self, license):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[8]/input').send_keys(license)
+
+    def enter_uploadposter(self):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[2]/div[2]/input').send_keys('C:/Users/samaneh/Pictures/Saved Pictures/OIP.jpg')
+
+    def submitpost(self):
+        self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[3]/button').click()
+
 
 
--- Tests/CreatePostValidTest.py
+++ Tests/CreatePostValidTest.py
@@ -14,21 +14,31 @@ class CreatePostTests(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.service = Service(executable_path=ChromeDriverManager().install())
         cls.options = Options()
-        cls.options.headless = True
+        cls.options.headless = False
         cls.driver = webdriver.Chrome(service=cls.service, options=cls.options)
-        cls.driver.implicitly_wait(3)
+        cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
     def test_valid_createpost(self):
         self.driver.get("https://sharedataset.com/account/login")
         login = Login(driver=self.driver)
-        main_page = MainPage(driver=self.driver)
         login.enter_username("samane_mss@yahoo.com")
         login.enter_password("123456S@vdash")
         login.click_on_continue_button()
+        main_page = CreatePost(driver=self.driver)
         main_page.check_main_page()
+        main_page.enter_title("Test")
+        main_page.select_source_powerbi()
+        main_page.enter_publicurl("https://community.powerbi.com/t5/Data-Stories-Gallery/Meat-to-eat-or-not-to-eat-Narrative-statistics/td-p/2177919")
+        main_page.enter_originalauthor("a9126030767")
+        main_page.enter_description("Test")
+        main_page.enter_tags("Test")
+        main_page.select_whocanseepost()
+        main_page.enter_license("Test")
+        main_page.enter_uploadposter()
+        main_page.submitpost()
 
-        sleep(30)
+        sleep(10)
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -36,4 +46,4 @@ class CreatePostTests(unittest.TestCase):
         cls.driver.quit()
 
 
-sleep(5)
+sleep(10)
