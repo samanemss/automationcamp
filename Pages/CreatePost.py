4e075a79cdebdb85068bc88143f572cf47d94096
Samaneh <samane_mss@yahoo.com>
2022-05-24 17:19:41 +0430

    Login.py SignUp.py Signupvatest.py CreatePost.py

--- Pages/CreatePost.py
+++ Pages/CreatePost.py
@@ -16,7 +16,17 @@ class CreatePost:
     def select_source_powerbi(self):
         source = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/select')
         drop = Select(source)
-        drop.select_by_value("powerbi")
+        drop.select_by_value(select_value_powerbi)
+
+    def select_source_tableau(self):
+        source = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/select')
+        drop_tableau = Select(source)
+        drop_tableau.select_by_value(select_value_tableau)
+
+    def select_source_arc(self):
+        source = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[2]/select')
+        drop_arc = Select(source)
+        drop_arc.select_by_value(select_value_arcgis)
 
     def enter_publicurl(self, publicurl):
         self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[3]/input').send_keys(publicurl)
@@ -30,10 +40,15 @@ class CreatePost:
     def enter_tags(self, tags):
         self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[6]/input').send_keys(tags)
 
-    def select_whocanseepost(self):
+    def select_whocanseepostonlyme(self):
         seepost = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[7]/select')
         dropseepost = Select(seepost)
         dropseepost.select_by_value("private")
+
+    def select_whocanseeposteverybody(self):
+        seepost = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[7]/select')
+        dropseepost = Select(seepost)
+        dropseepost.select_by_value("public")
 
     def enter_license(self, license):
         self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[8]/input').send_keys(license)
@@ -44,5 +59,7 @@ class CreatePost:
     def submitpost(self):
         self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div/div/div/div[3]/button').click()
 
+    def check_post_created(self):
+        self.driver.find_element(By.LINK_TEXT, go_to_post_button_link_text)
 
 
